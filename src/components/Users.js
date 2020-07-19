cfe85ed1c7485d7c7485fed858ead34a62448b34
github username <github email address>
2020-07-19 19:22:38 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 // add any needed imports here
+import { connect } from 'react-redux'
 class Users extends Component {
 
   render() {
+    let users = this.props.users.map ( (user, index) => 
+    <li key={index}>{user.username}</li>)
     return (
       <div>
+        {this.props.numberOfUsers}
         <ul>
-          Users!
+          {users}
           {/* Write code here that displays the usernames of all users in the Redux store */}
-          {/* In addition, display the total number of users curently in the store */}
+          {/* In addition, display the total number of users currently in the store */}
         </ul>
       </div>
     )
@@ -17,5 +21,24 @@ class Users extends Component {
 
 //add mapStateToProps here
 
+const mapStateToProps = state => {
+  return {
+    users: state.users,
+    numberOfUsers: state.users.length
+  }
+}
+
+/*
+oh oh, I think this is just taking the mapStateToProps function definition above 
+and the React/class component Users, and in one single line of code, AKA
+export default connect(mapStateToProps)(Users):
+
+1) uses connect keyword to pull state from the redux store
+2) uses mapStateToProps to structure state as a local (i.e., for this 
+component, Users) simplified object, and sets this.props to be that object
+3) clones and transforms Users into this new connected/polymerized react-redux component and exports it
+*/ 
+
 // connect this component to Redux
-export default Users
+export default connect(mapStateToProps)(Users)
+
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,15 +1,24 @@
+
+
 export default function manageUsers(state = {
   users: [],
 }, action){
   switch (action.type) {
     case 'ADD_USER':
+      // console.log so we can confirm that this is happening
       console.log('adding ', action.user);
+
+      // return new state, which gets stored in the store and overwrites previous state
       return {
+        // copy everything from previous state
         ...state,
+        // copy everything from previous users array, adding the new user at the end
         users: [...state.users, action.user]
       }
 
     default:
+      // if the app is initializing or the dispatched action 
+      // doesn't === 'ADD_USER' return state unmodified
       return state;
   }
 };
